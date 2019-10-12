df6d3d5f018aceb1a2833843530c44bb681f444f
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-12 13:32:24 -0500

    Set Assignment
    Final commit - function written, tests passing.

--- set_membership.py
+++ set_membership.py
@@ -9,7 +9,16 @@ if a value is in a set or False if it is not.
 
 
 def in_set(this_set, a_value):
-    pass
+    """
+    function to check if a value is in a set
+    :param this_set: set of numbers
+    :param a_value: value to check if in set
+    :return: True if value found or False if not
+    """
+    if a_value in this_set:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
